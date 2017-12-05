f60e7fe85c57d9c5d9b96bda8d8e6148f2a7249c
Michel González <michiwoo.web@gmail.com>
2017-12-05 00:32:05 -0600

    18. Directiva ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Homesquare';
   lugares:any = [
-    {active: true, nombre: 'Florería la Gardenia'},
-    {active: true, nombre: 'Donas la Pasadita'},
-    {active: true, nombre: 'Veterinaria Huellitas Felices'},
-    {active: false, nombre: 'Sushi Sushiroll'},
-    {active: true, nombre: 'Hotel la Gracia'},
-    {active: false, nombre: 'Zapatería el Clavo'}
+    {cercania: 1, distancia: 1, active: true, nombre: 'Florería la Gardenia'},
+    {cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la Pasadita'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
+    {cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
+    {cercania: 3, distancia: 120, active: false, nombre: 'Zapatería el Clavo'}
   ];
   lat:number = 20.9570718;
   lng:number = -97.4085713;
