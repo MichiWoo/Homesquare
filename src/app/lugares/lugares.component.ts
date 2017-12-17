4f982e81b66ac4ac8aefab4c25bb03528861a3d3
Michel González <michiwoo.web@gmail.com>
2017-12-17 13:07:48 -0600

    50. Configurando animaciones en nuestro proyecto

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
     selector: 'app-lugares',
-    templateUrl: './lugares.component.html'
+    templateUrl: './lugares.component.html',
+    animations: [
+        trigger('contenedorAnimable', [
+            state('inicial', style({
+                opacity: 0,
+                backgroundColor: 'green',
+                transform: 'rotate3d(0,0,0,0 deg)'
+            })),
+            state('final', style({
+                opacity: 1,
+                backgroundColor: 'yellow',
+                transform: 'rotate3d(5,10,20,30 deg)'
+            }))
+        ])
+    ]
 })
 export class LugaresComponent {
     title = 'HomeSquare';
+    state = 'inicial';
 
     lat:number = 20.9573458;
     lng:number = -97.4084174;
