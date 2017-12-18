e44d70a0d01a839e63f28121c4535ea0ec45b897
Michel González <michiwoo.web@gmail.com>
2017-12-18 00:13:19 -0600

    61. Registrando usuarios con firebase

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth/auth";
 
 @Injectable()
 
 export class AutorizacionService{
+  constructor(private angularFireAuth: AngularFireAuth) {
+
+  }
   public login = (email, password) => {
     console.log('Método de login')
   }
 
   public registro = (email, password) => {
-    console.log('Método de registro')
+    this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((response)=>{
+        console.log('Usuario Registrado con exito');
+        console.log(response);
+      })
+      .catch((error)=>{
+        console.log('Hubo un error');
+        console.log(error);
+      })
   }
 }
